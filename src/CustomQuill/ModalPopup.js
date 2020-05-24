dcc9933019f40b61a49d5d211157ecf26b76dd6c
masif7 <shadmanasif10@gmail.com>
2020-05-24 20:06:11 +0530

    Removed unused code

--- src/CustomQuill/ModalPopup.js
+++ src/CustomQuill/ModalPopup.js
@@ -5,13 +5,13 @@ class ModalPopup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        // modalOpen: false
+       
     };
   }
 
 
   render() {
-      const { modalOpen, handleClose, handleOpen}=this.props;
+      const { modalOpen, handleClose}=this.props;
     return (
       <Modal
         open={modalOpen}
@@ -35,78 +35,3 @@ class ModalPopup extends Component {
 }
 
 export default ModalPopup;
-//   render() {
-//     const {
-//       children,
-//       title,
-//       showModal,
-//       onClose,
-//       size,
-//       actionButtons,
-//       id,
-//       innerRef,
-//       scrolling,
-//       className,
-//     } = this.props;
-//     return (
-//       <Modal
-//         className={className}
-//         open={showModal}
-//         onClose={onClose}
-//         size={size}
-//         closeOnDimmerClick={false}
-//         id={`${id}`}
-//       >
-//         <div className="modal-loader" ref={innerRef} />{" "}
-//         <Modal.Header>{title}</Modal.Header>{" "}
-//         <Modal.Content scrolling={scrolling}>{children}</Modal.Content>{" "}
-//         <Modal.Actions>
-//           {" "}
-//           <CommonButton onClick={onClose} className="button-white-gradient">
-//             {" "}
-//             Close{" "}
-//           </CommonButton>{" "}
-//           {actionButtons &&
-//             actionButtons.map((item) => (
-//               <CommonButton
-//                 key={item.label}
-//                 onClick={() => item.callBack()}
-//                 className={item.btnClass || "button-blue-gradient"}
-//                 disabled={item.disabled}
-//               >
-//                 {" "}
-//                 {item.label}{" "}
-//               </CommonButton>
-//             ))}{" "}
-//         </Modal.Actions>{" "}
-//       </Modal>
-//     );
-//   }
-// }   
-// ModalPopup.propTypes = {
-//   showModal: PropTypes.bool,
-//   match: PropTypes.object,
-//   title: PropTypes.string,
-//   label: PropTypes.string,
-
-//   onClose: PropTypes.func,
-//   size: PropTypes.string,
-//   actionButtons: PropTypes.array,
-//   id: PropTypes.string,
-//   innerRef: PropTypes.oneOfType([PropTypes.element, PropTypes.object]),
-//   scrolling: PropTypes.bool,
-//   className: PropTypes.string,
-// };
-// ModalPopup.defaultProps = {
-//   showModal: false,
-//   onClose: () => {},
-//   size: "large",
-//   actionButtons: [],
-//   id: "box_modal",
-//   scrolling: true,
-//   className: "box_modal",
-// };
-
-// export default React.forwardRef((props, ref) => (
-//   <ModalPopup innerRef={ref} {...props} />
-// ));
